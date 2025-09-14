e4c01e0fca7b83117af3e1a0ddddd25d91890bb2
trilvid <kelvinchukwuebuka385@gmail.com>
2025-09-14 16:21:58 +0100

    working on front end

--- src/components/copytrade/Copytrade.jsx
+++ src/components/copytrade/Copytrade.jsx
@@ -12,9 +12,9 @@ const Copytrade = () => {
                 <span className="header-line"></span>
                 <h2 >what we do at</h2>
                 </div>
-                <h1 data-aos="fade-up" className='copytrade-header'>Copixlab</h1>
+                <h1 data-aos="fade-up" className='copytrade-header'>Mirrorstat</h1>
                 <p data-aos="fade-up">
-                At Copixlab, we provide accurate forex analysis to help investors maximize profits through copy trading. Using MT4 and MT5, our experts analyze the markets with technical and fundamental strategies, identifying high-probability trades. We share real-time signals, market updates, and risk assessments directly through our Telegram channel, allowing investors to copy top-performing trades effortlessly.
+                At Mirrorstat, we provide accurate forex analysis to help investors maximize profits through copy trading. Using MT4 and MT5, our experts analyze the markets with technical and fundamental strategies, identifying high-probability trades. We share real-time signals, market updates, and risk assessments directly through our Telegram channel, allowing investors to copy top-performing trades effortlessly.
                   </p>
         </div>
         <div className="about-page-img forex-img-container">
